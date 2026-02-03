f1d4058302cbc7191d723914fe51f096d2240c46
Daniel <singer.daniel85@gmail.com>
2026-02-03 18:35:26 +0100

    item can be edited and created

--- backend/src/controllers/item/getItem.ts
+++ backend/src/controllers/item/getItem.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { Item } from '../../models/itemModel';
+import { convertCentsToEuro } from '../../util/conversions/currency';
 
 export const getItem = async (
   req: Request,
@@ -8,10 +9,27 @@ export const getItem = async (
   next: NextFunction,
 ) => {
   try {
-    const item = await Item.findOne({
+    const result = await Item.findOne({
       _id: req.params.id,
       // ...baseFilter(req),
     });
+
+    if (!result) {
+      res.status(404);
+      throw new Error('Item not found');
+    }
+
+    const item = {
+      ...result.toObject(),
+      purchase: {
+        ...result.purchase,
+        price: convertCentsToEuro(result.purchase?.price!),
+      },
+      sell: {
+        ...result.sell,
+        price: convertCentsToEuro(result.sell?.price!),
+      },
+    };
     res.status(StatusCodes.OK).json(item);
   } catch (error) {
     next(error);
--- backend/src/util/conversions/currency.ts
+++ backend/src/util/conversions/currency.ts
@@ -1,2 +1,4 @@
 export const convertEuroToCents = (euro: number) => euro * 100;
-export const convertCentsToEuro = (cents: number) => cents / 100;
+export const convertCentsToEuro = (cents: number | undefined) => {
+  return cents ? cents / 100 : null;
+};
