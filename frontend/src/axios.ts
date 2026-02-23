e4df595ef3f62be8a2208393cb120fe8da188b92
Daniel <singer.daniel85@gmail.com>
2026-02-23 20:37:48 +0100

    interceptor added

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,19 +1,17 @@
 import axios from "axios";
 
-// axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
-
 const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// api.interceptors.request.use((config) => {
-//   const accessToken = localStorage.getItem("authToken");
-//   if (accessToken) {
-//     config.headers.Authorization = `Bearer ${accessToken}`;
-//   }
-//   return config;
-// });
+api.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem("authToken");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
 
 export default api;
