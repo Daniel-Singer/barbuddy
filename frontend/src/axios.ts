1b451b2b2af2b8600e13948ef09e68767a5f4135
Daniel <singer.daniel85@gmail.com>
2026-02-23 18:57:46 +0100

    axios instance updated

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -2,7 +2,13 @@ import axios from "axios";
 
 // axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
 
-axios.interceptors.request.use((config) => {
+const api = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
   const accessToken = localStorage.getItem("authToken");
   if (accessToken) {
     config.headers.Authorization = `Bearer ${accessToken}`;
@@ -10,8 +16,4 @@ axios.interceptors.request.use((config) => {
   return config;
 });
 
-export default axios.create({
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+export default api;
